5bc675a3ed50c256713d355427cc2e029ea023ee
Risa Harada <supikateriburu@gmail.com>
2017-06-22 01:37:31 +0900

    #

--- risacan/binary_search.rb
+++ risacan/binary_search.rb
@@ -1,4 +1,6 @@
 def binary_search(list, item)
+  puts "はじめ！"
+  puts "#{item}を探すぞ!!"
   # index
   low = 0
   high = list.size - 1
@@ -16,22 +18,34 @@ def binary_search(list, item)
     step += 1
     puts step
   end
+  puts "おわり！"
 end
 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array = [1, 2, 4, 5, 7, 8, 9, 10]
 binary_search(array, 10)
+puts
 binary_search(array, 7)
+puts
 binary_search(array, -1)
+puts
 binary_search(array, 11)
+puts
 
 # p9 練習問題 1.1
+Math.log(128, 2) # 7
+puts
 array = (1..128).to_a
 binary_search(array, 1)
+puts
 binary_search(array, 128)
-Math.log(128, 2) # 7
+puts
+
 
 # p9 練習問題 1.2
 Math.log(128*2, 2) # 8
+puts
 array = (1..128*2).to_a
 binary_search(array, 1)
+puts
 binary_search(array, 128*2)
+puts
